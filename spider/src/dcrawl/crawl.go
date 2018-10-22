9012c06ebba05674c3b302402fea7a5a9746d902
丁敬 <dingjing@live.cn>
2018-10-22 11:48:20 +0800

    整理 get 请求 代码

--- spider/src/dcrawl/crawl.go
+++ spider/src/dcrawl/crawl.go
@@ -9,10 +9,13 @@
 package dcrawl
 
 import (
+	"crypto/tls"
+	"io"
 	"io/ioutil"
 	"library/goquery"
 	"library/mahonia"
 	"net/http"
+	"net/http/cookiejar"
 	"strings"
 	"sync"
 )
@@ -31,67 +34,30 @@ type SpiderContent struct {
 
 var SpiderGroup = sync.WaitGroup{}
 
-// 根据 url 获取页面 html 字符串
-func GetHTMLByUrl(url *string, head *map[string]string) (bool, string) {
-	charset := "utf8"
-	ret := true
 
-RET:
+/* 获取 url get 请求 */
+func GetHTMLByUrl(url string, head *map[string]string) (bool, string) {
+
+	ret := false
+	html := ""
+
+	// 获取请求类型
+	rtp, _ := CheckHTTP(url)
+	if "https" == rtp {
+		ret, html = GetHTTPSRequest(url, head)
+	} else {
+		ret, html = GetHTTPRequest(url, head)
+	}
+
 	if !ret {
-		return ret, ""
+		Log.Errorf("url: %s 请求错误!", url)
 	}
 
-	if (nil == url) || ("" == *url) {
-		goto RET
-	}
-
-	client := &http.Client{}
-	request, err := http.NewRequest("GET", *url, nil)
-	if nil != err {
-		Log.Errorf("错误的request get: %s|%s", url, err)
-		ret = false
-		goto RET
-	}
-	if nil != head {
-		for k, v := range *head {
-			request.Header.Add(k, v)
-		}
-	}
-
-	resp, err := client.Do(request)
-	if (nil != err) || (200 != resp.StatusCode) {
-		ret = false
-		Log.Errorf("无法访问url: %s|%s\n", url, err)
-		goto RET
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if nil != err {
-		ret = false
-		Log.Errorf("byte转string失败:%s\n", url)
-		goto RET
-	}
-
-	doc, err := goquery.NewDocumentFromReader(strings.NewReader(string(body)))
-	if nil != err {
-		ret = false
-		Log.Errorf("HTML转string失败%s\n", url)
-		goto RET
-	}
-
-	// 获取页面编码
-	ic, ret := doc.Find("head").Find("meta[http-equiv]").Attr("content")
-	if ret {
-		ic = strings.TrimSpace(ic)
-		ict := strings.Split(ic, "=")
-		ic = ict[len(ict)-1]
-		if "" != ic {
-			charset = ic
-		}
-	}
-
-	return ret, ConvertToString(string(body), charset, "utf8")
+	return ret, html
 }
+
+
+
 
 // 根据 url 获取页面 html 字符串
 func Post(url *string, head *map[string]string, para *map[string]string) (bool, string) {
@@ -160,11 +126,172 @@ RET:
 	return ret, ConvertToString(string(body), charset, "utf8")
 }
 
+
+
+
+
+
+
+
+
+
+/* 请求 http 返回 html */
+func GetHTTPRequest (url string, head *map[string]string) (bool, string) {
+	ret := false
+	body := ""
+
+	client := &http.Client{}
+	request, err := http.NewRequest("GET", url, nil)
+	if nil != err {
+		Log.Errorf("错误的 get 请求: %s|%s", url, err)
+	}
+	if nil != head {
+		for k, v := range *head {
+			request.Header.Add(k, v)
+		}
+	}
+	resp, err := client.Do(request)
+	if (nil != err) || (200 != resp.StatusCode) {
+		ret = false
+		Log.Errorf("url访问错误: %s|%s\n", url, err)
+	} else {
+		ret = true
+	}
+	defer resp.Body.Close()
+
+	if ret {
+		err, html := ReadByteToString(&(resp.Body))
+		if err {
+			body = ConvertToString(string(html), GetHTMLCharset(&html), "utf8")
+		} else {
+			ret = false
+		}
+	}
+
+	return ret, body
+}
+
+/* 请求 https 返回 html */
+func GetHTTPSRequest (url string, head *map[string]string) (bool, string) {
+	ret := false
+	body := ""
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	cookieJar, _ := cookiejar.New(nil)
+	client := &http.Client{
+		Jar:       cookieJar,
+		Transport: tr,
+	}
+
+	request, err := http.NewRequest("GET", url, nil)
+	if nil != err {
+		Log.Errorf("错误的 get 请求: %s|%s", url, err)
+	}
+	if nil != head {
+		for k, v := range *head {
+			request.Header.Add(k, v)
+		}
+	}
+	resp, err := client.Do(request)
+	if (nil != err) || (200 != resp.StatusCode) {
+		ret = false
+		Log.Errorf("url访问错误: %s|%s\n", url, err)
+	} else {
+		ret = true
+	}
+	defer resp.Body.Close()
+
+	if ret {
+		tret, html := ReadByteToString(&(resp.Body))
+		if tret {
+			body = ConvertToString(string(html), GetHTMLCharset(&html), "utf8")
+		} else {
+			ret = false
+		}
+	}
+
+	return ret, body
+}
+
+
+/* 没有任何依赖的工具函数 */
+
+/* 从 []byte 读取数据并转为 strig */
+func ReadByteToString(bt *io.ReadCloser) (bool, string) {
+	ret := false
+	body := ""
+
+	tmp, err := ioutil.ReadAll(*bt)
+	if nil != err {
+		ret = false
+		Log.Errorf("io读取错误！%s", err)
+	} else {
+		ret = true
+		body = string(tmp)
+	}
+
+	return ret, body
+}
+
+/* 获取页面的字符编码，返回字符编码 */
+func GetHTMLCharset (html *string) string {
+	ret := false
+	cs := "utf8"
+
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(string(*html)))
+	if nil == err {
+		ret = true
+	}
+
+	if ret {
+		ic, errs := doc.Find("head").Find("meta[http-equiv]").Attr("content")
+		if errs {
+			ic = strings.TrimSpace(ic)
+			ict := strings.Split(ic, "=")
+			ic = ict[len(ict)-1]
+			if "" != ic {
+				cs = ic
+			}
+		}
+	}
+
+	return cs
+}
+
+/* 检查请求类型，是 http 还是 https，返回请求类型和 url */
+func CheckHTTP (url string) (string, string) {
+	reqType := "http"
+	arr := strings.SplitN(url, "://", -1)
+	if (len(arr) >= 2) && ("https" == arr[0]) {
+		reqType = "https"
+	}
+
+	return reqType, url
+}
+
+/* 字符串编码转换 */
 func ConvertToString(src string, srcCode string, tagCode string) string {
-	srcCoder := mahonia.NewDecoder(srcCode)
-	srcResult := srcCoder.ConvertString(src)
-	tagCoder := mahonia.NewDecoder(tagCode)
-	_, cdata, _ := tagCoder.Translate([]byte(srcResult), true)
-	result := string(cdata)
+	result := ""
+	// 修正网站上编码字符串错误问题
+	codes := map[string]string {
+		"gb2312": "gbk",
+	}
+
+	if v, exits := codes[srcCode]; exits {
+		srcCode = v
+	}
+
+	if v, exits := codes[tagCode]; exits {
+		tagCode = v
+	}
+
+	srcResult := mahonia.NewDecoder(srcCode).ConvertString(src)
+	_, cdata, err := mahonia.NewDecoder(tagCode).Translate([]byte(srcResult), true)
+	if nil == err {
+		result = string(cdata)
+	}
+
 	return result
 }
--- spider/src/main/main.go
+++ spider/src/main/main.go
@@ -2,78 +2,61 @@ package main
 
 import (
 	"dcrawl"
-	"dspider"
+	"fmt"
 )
 
 /**
  * 主函数实现保存操作，其它地方都是解析
- *
- *
+ * 一个爬虫，一个线程
  */
 
-var MI = dcrawl.SMongoInfo{"127.0.0.1", 27017, "", "", "novel_online", "online"}
+//var MI = dcrawl.SMongoInfo{"127.0.0.1", 27017, "", "", "novel_online", "online"}
 
-var SC = map[*dcrawl.SpiderContent]dcrawl.SpiderRun{}
+//var SC = map[*dcrawl.SpiderContent]dcrawl.SpiderRun{}
 
 func main() {
 	/* mzhu8 爬虫 */
-	mzhu8 := dcrawl.SpiderContent{}
-	mzhu8.SpiderName = "mzhu8"
-	mzhu8.BaseUrl = "http://www.mzhu8.com"
-	mzhu8.MI = MI
-	mzhu8.SeedUrl = map[string]int{
-		//"http://www.mzhu8.com/mulu/18/":1,
-		"http://www.mzhu8.com/mulu/1/":  8,
-		"http://www.mzhu8.com/mulu/2/":  8,
-		"http://www.mzhu8.com/mulu/3/":  3,
-		"http://www.mzhu8.com/mulu/5/":  5,
-		"http://www.mzhu8.com/mulu/7/":  13,
-		"http://www.mzhu8.com/mulu/6/":  51,
-		"http://www.mzhu8.com/mulu/16/": 36,
-	}
+	//mzhu8 := dcrawl.SpiderContent{}
+	//mzhu8.SpiderName = "mzhu8"
+	//mzhu8.BaseUrl = "http://www.mzhu8.com"
+	//mzhu8.MI = MI
+	//mzhu8.SeedUrl = map[string]int{
+	//	//"http://www.mzhu8.com/mulu/18/":1,
+	//	"http://www.mzhu8.com/mulu/1/":  8,
+	//	"http://www.mzhu8.com/mulu/2/":  8,
+	//	"http://www.mzhu8.com/mulu/3/":  3,
+	//	"http://www.mzhu8.com/mulu/5/":  5,
+	//	"http://www.mzhu8.com/mulu/7/":  13,
+	//	"http://www.mzhu8.com/mulu/6/":  51,
+	//	"http://www.mzhu8.com/mulu/16/": 36,
+	//}
+
+	/* 顶点小说 */
+	//booktxt := dcrawl.SpiderContent{}
+	//booktxt.SpiderName = "booktxt"
+	//booktxt.BaseUrl = "http://www.mzhu8.com"
+	//booktxt.MI = MI
+	//booktxt.SeedUrl = map[string]int{
+	//}
 
 	/* 添加爬虫 */
-	SC[&mzhu8] = dspider.Mzhu8Run
+	//SC[&mzhu8] = dspider.Mzhu8Run
+	//SC[&booktxt] = dspider.Mzhu8Run
 
 	/* 开始运行爬虫 */
-	for spp, spf := range SC {
-		dcrawl.SpiderGroup.Add(1)
-		go spf(spp)
-	}
+	//for spp, spf := range SC {
+	//	dcrawl.SpiderGroup.Add(1)
+	//	go spf(spp)
+	//}
 
 	/* 等待所有爬虫执行完毕 */
-	dcrawl.SpiderGroup.Wait()
+	//dcrawl.SpiderGroup.Wait()
 
-	//c := make(chan int, 10)
-	//group := sync.WaitGroup{}
-	//c <- 1
-	//c <- 2
-	//c <- 3
-	//close(c)
-	//group.Add(2)
-	//go func() {
-	//	for {
-	//		i, isClose := <-c
-	//		if !isClose {
-	//			fmt.Println("channel closed!")
-	//			group.Done()
-	//			return
-	//		}
-	//		fmt.Println(i)
-	//	}
-	//}()
-	//
-	//go func() {
-	//	for {
-	//		i, isClose := <-c
-	//		if !isClose {
-	//			fmt.Println("channel closed!")
-	//			group.Done()
-	//			return
-	//		}
-	//		fmt.Println(i)
-	//	}
-	//}()
-	//group.Wait()
+
+
+	ret, html := dcrawl.GetHTMLByUrl("http://www.baiddu.com", nil)
+	fmt.Println(ret)
+	fmt.Println(html)
+
 
 }
